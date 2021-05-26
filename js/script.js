a4650b87e0fc475941fa1971a42a5bd0951679bb
Marco Tessitore <marcotexcode@gmail.com>
2021-05-26 14:13:27 +0200

    add: esercizio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
 
 
-console.log('hello World');
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colore = prompt('Inserisci il tuo colore preferito');
+
+document.write(nome + cognome + colore + '21');
