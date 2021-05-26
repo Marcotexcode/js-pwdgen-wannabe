7d797f6e9e2987ea444f42c79fcdc61d089bf19e
Marco Tessitore <marcotexcode@gmail.com>
2021-05-26 15:10:50 +0200

    add: bonusPersonale

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ cognome = cognome.toLowerCase();
 colore = colore.toLowerCase();
 
 document.write(nome + cognome + colore + '21');
+
+
+
+// BONUS PERSONALE
+var pass = nome + cognome + colore + '21';
+
+document.getElementById('password').innerHTML = pass;
+
