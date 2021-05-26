6767987d7fd7c38f12d717be08212f028a1549a7
Marco Tessitore <marcotexcode@gmail.com>
2021-05-26 14:22:43 +0200

    add: bonus

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 var colore = prompt('Inserisci il tuo colore preferito');
 
+nome = nome.toLowerCase();
+cognome = cognome.toLowerCase();
+colore = colore.toLowerCase();
+
 document.write(nome + cognome + colore + '21');
